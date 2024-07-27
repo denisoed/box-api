a57657a4522f9c24fc2dfa081401a83c5f24bd49
Denisoed <denisoed@gmal.com>
2024-07-27 11:42:27 +0600

    init

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const logger = new Logger();
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT;
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
+
+  logger.log(`Application listening on port ${port}`);
+}
+bootstrap();
