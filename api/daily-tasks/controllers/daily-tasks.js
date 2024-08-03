68268d3e2580b1b110aece82fbf482541a662cea
Denisoed <denisoed@gmal.com>
2024-08-03 15:44:46 +0600

    Added done & ready fields

--- api/daily-tasks/controllers/daily-tasks.js
+++ api/daily-tasks/controllers/daily-tasks.js
@@ -5,9 +5,29 @@
  * to customize this controller
  */
 
-const { formatError, calcDailyScore } = require('../../../helpers')
+const { formatError, calcDailyScore, checkDailyTaskDone } = require('../../../helpers')
 
 module.exports = {
+
+  async find(ctx) {
+    const user = await strapi.query('user', 'users-permissions').findOne({ id: ctx.state.user.id });
+    if (!user) {
+      return ctx.badRequest(
+        null,
+        formatError({
+          id: 'Auth.form.error.user.notFound',
+          message: 'User not found',
+        })
+      );
+    }
+    const tasks = await strapi.query('daily-tasks').find(ctx.query);
+    const formattedTasks = tasks.map(task => ({
+      ...task,
+      ready: +user.dailyScore >= +task.goal,
+      done: checkDailyTaskDone(user, task.type),
+    }))
+    return formattedTasks;
+  },
   async claimReward(ctx) {
     const { taskType } = ctx.request.body;
     const user = await strapi.query('user', 'users-permissions').findOne({ id: ctx.state.user.id });
--- helpers/index.js
+++ helpers/index.js
@@ -7,8 +7,17 @@ function calcDailyScore(user, newScore) {
   return +newScore
 }
 
+function checkDailyTaskDone(user, taskType) {
+  const today = new Date();
+  const lastUpdate = new Date(user.updated_at);
+  if (lastUpdate.getDate() === today.getDate()) {
+    return user.tasks.some(task => task.type === taskType);
+  }
+  return false;
+}
+
 const formatError = (error) => [
   { messages: [{ id: error.id, message: error.message, field: error.field }] },
 ];
 
-module.exports = { calcDailyScore, formatError }
+module.exports = { calcDailyScore, formatError, checkDailyTaskDone }
