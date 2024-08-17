4e0d79339c331e242342e60457eac8b1e073ab66
Denisoed <denisoed@gmal.com>
2024-08-17 14:20:19 +0600

    Added buyBooster method

--- api/boosters/controllers/boosters.js
+++ api/boosters/controllers/boosters.js
@@ -5,4 +5,49 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async buyBooster(ctx) {
+    const { boosterType } = ctx.request.body;
+    const user = await strapi.query('user', 'users-permissions').findOne({ id: ctx.state.user.id });
+    if (!user) {
+      return ctx.badRequest(
+        null,
+        formatError({
+          id: 'Auth.form.error.user.notFound',
+          message: 'User not found',
+        })
+      );
+    }
+    const booster = await strapi.query('boosters').findOne({ type: boosterType });
+    if (!booster) {
+      return ctx.badRequest(
+        null,
+        formatError({
+          id: 'Auth.form.error.booster.notFound',
+          message: 'Booster not found',
+        })
+      );
+    }
+    if (user.boosters.some(b => b.type === booster.type)) {
+      return ctx.badRequest(
+        null,
+        formatError({
+          id: 'Auth.form.error.booster.alreadyClaimed',
+          message: 'Booster already buyed',
+        })
+      );
+    }
+    const newComponentBooster = {
+      __component: 'boosters.booster',
+      type: booster.type,
+      roundsLeft: booster.rounds,
+      reward: booster.reward
+    };
+    user.score = user.score - booster.price;
+    user.boosters = [...user.boosters, newComponentBooster];
+    await strapi
+      .query('user', 'users-permissions')
+      .update({ id: user.id }, user);
+    return ctx.send({ success: true, data: booster });
+  },
+};
